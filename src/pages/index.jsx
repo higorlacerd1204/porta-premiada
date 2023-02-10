0c8dec4f210fee42f8065fb3a8a59923cdc0a3c0
higorlacerd1204 <higor.agatha@gmail.com>
2023-02-09 21:39:34 -0300

    Adiciona componente Porta e integra com ts

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,13 @@
-import Presente from 'components/presente';
+import Porta from 'components/porta';
+import { useState } from 'react';
+import PortaModel from '../../model/porta';
 
 export default function Home() {
+  const [p1, setP1] = useState(new PortaModel(1));
+
   return (
-    <>
-      <Presente />
-    </>
+    <div style={{ display: 'flex' }}>
+      <Porta porta={p1} />
+    </div>
   );
 }
